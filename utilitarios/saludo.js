6f296c43de415ff81b273b58c255635f831555ac
Angel-DGP <angel.patinogd@gmail.com>
2024-08-06 14:23:20 -0500

    Funciones de convertirINT, convertirFloat OK

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -6,5 +6,19 @@ return ving
 saludar = function(){
     let name = recuperartxt("txtname")
     let lname= recuperartxt("txtlname")
-    console.log("Hola "+ name+" " + lname)
+    let edad = converInt("txtdad")
+    let estatura = converfloat("txtesta")
+    console.log("Hola "+ name+" " + lname+ ", tu edad es: " + edad + " y estatura: " + estatura)
+}
+
+converInt = function(idcmp){
+    let vSTR = recuperartxt(idcmp)
+    let vINT = parseInt(vSTR)
+    return vINT
+}
+
+converfloat = function(idcmp){
+    let vSTR = recuperartxt(idcmp)
+    let vFloat = parseFloat(vSTR)
+    return vFloat
 }
