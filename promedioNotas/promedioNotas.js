d1cc757c4cc5e90e53f416dcac5a6cd5b1dd9f98
Angel-DGP <angel.patinogd@gmail.com>
2024-08-07 23:29:00 -0500

    Mensaje sobre notas implementados

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -7,14 +7,18 @@ calcularPromedioNotas = function(){
     console.log(promedio)
     if (promedio>8&&promedio<=10){
         mostrarimg("idimg","./img/exito.gif")
+        mostrartxt("lblres", "Excelente")
     }
     else if(promedio<=8&&promedio>=5){
         mostrarimg("idimg","./img/casiexito.gif")
+        mostrartxt("lblres", "Buen Trabajo")
     }
     else if(promedio<5&&promedio>0){
         mostrarimg("idimg","./img/fracaso.gif")
+        mostrartxt("lblres", "Reprobado")
     }
     else{
         mostrarimg("idimg","./img/casifracaso.gif")
+        mostrartxt("lblres", "Datos Incorrectos")
     }
 }
