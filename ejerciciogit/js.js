547a4e5d7d75808836f468f403146a80df5d21e2
Angel-DGP <angel.patinogd@gmail.com>
2024-08-10 22:44:35 -0500

    Archivos desplazados a carpeta Parte-1

--- ejerciciogit/js.js
+++
@@ -1,6 +0,0 @@
-saludar = function(){
-    let imgcmp
-    imgcmp = document.getElementById("idnaid")
-    imgcmp.src= "./img/naidlose.gif"
-    console.log("dentro de funcion saludar")
-}
--- promedioNotas/serviciosPromedioNotas.js
+++
@@ -1,4 +0,0 @@
-calcularPromedio = function(n1,n2,n3){
-    let promedio = (n1 + n2 + n3)/3
-    return promedio.toFixed(2)
-}
