7a6c641e36f70d95a086900cc190b979f5f87553
Angel-DGP <angel.patinogd@gmail.com>
2024-08-16 20:02:03 -0500

    EJERCICIOS BUCLES/FOR IMPLEMENTADOS

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,50 @@
+mostrarNumeros = function(){
+    console.log("antes del for")
+    for(let i=0; i<4;i++){
+        console.log(i)
+    }
+    console.log("Despues del for")
+}
+
+mostrarNumeros2 = function(){
+    console.log("ANTES DEL FOR")
+    for (let indice = 1;indice<=5;indice++){
+        console.log(indice)
+
+    }
+    console.log("DESPUES DEL FOR")
+
+}
+mostrarPares = function(){
+    console.log("ANTES DEL FOR")
+    for(let x = 2; x<=10;x+=2){
+        console.log(x)
+    }
+    console.log("DESPUES DEL FOR")
+}
+mostrarInverso = function(){
+    console.log("ANTES DEL FOR")
+    for (let i = 10;i>0;i--){
+        console.log(i)
+    }
+    console.log("DESPUES DEL FOR")
+
+}
+
+hackearNasaPelis = function(){
+    console.log("ANTES DEL FOR")
+    for(let porcentaje = 0;porcentaje<=100;porcentaje+=10){
+        console.log("HACKEANDO NASA " + porcentaje+ "% ")
+
+    }
+    console.log("DESPUES DEL FOR")
+    console.log("LA NASA HA SIDO HACKEADA")
+}
+
+mostrarImpares = function(){
+    console.log("ANTES DEL FOR")
+    for (let x = 1;x<22;x+=2){
+        console.log(x)
+    }
+    console.log("DESPUES DEL FOR")
+}
