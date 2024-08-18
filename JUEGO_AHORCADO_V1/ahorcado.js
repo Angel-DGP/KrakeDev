ca8d21e284545f36a24e8253eab61f8420a3b077
Angel-DGP <angel.patinogd@gmail.com>
2024-08-17 22:30:56 -0500

    PASO 3 SUBIDO

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,6 +1,7 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 //La paciencia no es mi virtud, pero la logica si
 let palabraSecreta
+let letrasEncontradas
 
 esMayuscula = function(caracter){
     codigo = caracter.charCodeAt(0)
@@ -13,8 +14,8 @@ guardarPalabra = function(){
     palabraSecreta = recuperarTexto("txtSecreta")
     if(palabraSecreta.length==5){
         for(let i = 0; i<=4;i++){
-            letra = palabraSecreta.charAt(i)
-            if(esMayuscula(letra)==false){errorP1 = true}
+            letraIterada = palabraSecreta.charAt(i)
+            if(esMayuscula(letraIterada)==false){errorP1 = true}
         }
         if(errorP1==false){console.log(palabraSecreta)}
     }
@@ -28,3 +29,15 @@ mostrarLetra = function(letra,posicion){
     if(posicion==3){mostrarTexto("div3",letra)}
     if(posicion==4){mostrarTexto("div4",letra)}
 }
+
+validar = function(letra){
+    for(let i = 0; i<=palabraSecreta.length-1;i++){
+        letraIterada = palabraSecreta.charAt(i)
+        if(letraIterada==letra){
+            letrasEncontradas+=1
+            mostrarLetra(letra,i)
+        
+        }
+    }
+}
+
