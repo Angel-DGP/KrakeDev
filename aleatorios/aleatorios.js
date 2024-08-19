5010db0347679c144261422d7549acc5ad8c933d
Angel-DGP <angel.patinogd@gmail.com>
2024-08-18 19:14:18 -0500

    PASO 5 Y 6 REALIZADOS

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -1,28 +1,29 @@
 numeroAleatorio = function () {
-  return (aleatorio = parseInt(Math.random() * 100) + 1)
-}
+  return (aleatorio = parseInt(Math.random() * 100) + 1);
+};
 
 generarAleatorios = function () {
-  let aleatorios = []
-  let numeroU = recuperarTexto("txtNumero")
-  let NumeroA
+  let aleatorios = [];
+  let numeroU = recuperarTexto("txtNumero");
+  let NumeroA;
   if (numeroU <= 20 && numeroU >= 5) {
     for (let i = 0; i < numeroU; i++) {
-      NumeroA = numeroAleatorio()
-      aleatorios.push(NumeroA)
+      NumeroA = numeroAleatorio();
+      aleatorios.push(NumeroA);
     }
+    mostrarResultados(aleatorios);
   }
-}
+};
 
-mostrarResultados = function(arregloNumeros){
-    let cmp = document.getElementById("divTabla")
-    let contenidoTabla="<table><tr><th>TABLA</th></tr>"
-    for(let i=0;i<arregloNumeros.length;i++){
-        console.log(arregloNumeros[i])
-        contenidoTabla+="<tr><td>"
-        contenidoTabla+=arregloNumeros[i]
-        contenidoTabla+="</td></tr>"
-    }
-    contenidoTabla+="</table>"
-    cmp.innerHTML=contenidoTabla
-}
+mostrarResultados = function (arregloNumeros) {
+  let cmp = document.getElementById("divTabla");
+  let contenidoTabla = "<table><tr><th>TABLA</th></tr>";
+  for (let i = 0; i < arregloNumeros.length; i++) {
+    console.log(arregloNumeros[i]);
+    contenidoTabla += "<tr><td>";
+    contenidoTabla += arregloNumeros[i];
+    contenidoTabla += "</td></tr>";
+  }
+  contenidoTabla += "</table>";
+  cmp.innerHTML = contenidoTabla;
+};
