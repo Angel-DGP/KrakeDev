708b12161f1027984b95606090cd9c96137e25aa
Angel-DGP <angel.patinogd@gmail.com>
2024-08-18 19:03:41 -0500

    PASO 3 REALIZADO

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -1,3 +1,15 @@
-numeroAleatorio = function(){
-    return aleatorio = parseInt(Math.random() * 100) + 1
+numeroAleatorio = function () {
+  return (aleatorio = parseInt(Math.random() * 100) + 1)
+}
+
+generarAleatorios = function () {
+  let aleatorios = []
+  let numeroU = recuperarTexto("txtNumero")
+  let NumeroA
+  if (numeroU <= 20 && numeroU >= 5) {
+    for (let i = 0; i < numeroU; i++) {
+      NumeroA = numeroAleatorio()
+      aleatorios.push(NumeroA)
+    }
   }
+}
